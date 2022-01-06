71ccc115131f402f1d8c0f1e36144068fcc6cc96
Lazar Dejanovic <lazzar99@gmail.com>
2022-01-06 17:20:13 +0100

    Initial commit

---
+++ src/main/java/com/sk02/sk02_eureka_service/Sk02EurekaServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sk02.sk02_eureka_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sk02EurekaServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sk02EurekaServiceApplication.class, args);
+    }
+
+}
