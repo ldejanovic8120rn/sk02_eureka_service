645a417b04dba4dbce1e24b1c2a89419e647c65e
VukV <vuk.vukovic.2000@gmail.com>
2022-01-06 17:34:30 +0100

    gmta

--- src/main/java/com/sk02/sk02_eureka_service/Sk02EurekaServiceApplication.java
+++ src/main/java/com/sk02/sk02_eureka_service/Sk02EurekaServiceApplication.java
@@ -2,8 +2,10 @@ package com.sk02.sk02_eureka_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class Sk02EurekaServiceApplication {
 
     public static void main(String[] args) {
